7316aefc654164d6b4e1e57237ad9a4ef4d5822d
dsitweed <luffy3042001@gmail.com>
2022-03-20 11:26:36 +0700

    use muler to store images

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import multer from "multer";
 import 'dotenv/config';
 
 import auth from './routes/auth.js';
@@ -11,6 +12,19 @@ import categories from './routes/categories.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 const URI = process.env.MDB_URI;
+// set images folder save
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 app.use(bodyParser.json({limit:'30mb'}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
@@ -29,4 +43,8 @@ mongoose.connect(URI)
 app.use('/api/auth', auth);
 app.use('/api/users', users);   
 app.use('/api/posts', posts);
-app.use('/api/category', categories);   
+app.use('/api/category', categories);
+
+app.post('/api/upload', upload.single("file"), (req,res) => {
+    res.status(200).json("File has been uploaded");
+});
